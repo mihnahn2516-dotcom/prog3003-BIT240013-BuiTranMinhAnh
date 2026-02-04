bcc5f0d560669a251a872438fe7c037e392b390e
Minh Anh <mihnahn2516@gmail.com>
2026-02-04 16:54:57 +0700

    Add files via upload

---
+++ Bai2.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ex2;
+
+class NumberThread extends Thread {
+    public NumberThread(String name) {
+        super(name);
+    }
+
+    @Override
+    public void run() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(getName() + ": " + i);
+        }
+    }
+}
+public class Bai2 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            new NumberThread("Thread-" + i).start();
+        }
+    }
+}
+
